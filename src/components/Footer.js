2bc3748f590f76a8ef47a003d525b0cb2364617e
Menjith P <70083973+menjithp@users.noreply.github.com>
2023-09-01 17:28:09 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+  return (
+      <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400"></span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        
+              <li>
+                  <a href="https://www.linkedin.com/in/menjith/" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
+              </li>
+
+              
+              <li>
+                  <a href="https://www.Github.com/in/menjithp/" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">GitHub</a>
+              </li>
+              
+
+            
+          </ul>
+      </footer>
+  )
+}
